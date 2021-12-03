a1170033d05aede278612569f5c975db4596f0c9
Rick Rackley <93223405+safehaven1017@users.noreply.github.com>
2021-12-03 18:37:13 -0500

    completed assignment

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 const incompleteList = document.querySelector("#incomplete-items");
 const completeList = document.querySelector("#complete-items");
 
+for (let i = 0; i < todoItems.length; i++) {
+    if (todoItems[i].completed)
+        addListItem(todoItems[i].title, "complete-item", completeList);
+    else
+        addListItem(todoItems[i].title, "incomplete-item", incompleteList);
+}
+
 function addListItem (textContent, liClass, parentList) {
     const newListItem = document.createElement("li");
     newListItem.setAttribute("class", liClass);
     newListItem.innerHTML = textContent;
     parentList.appendChild(newListItem);
-    
 }
