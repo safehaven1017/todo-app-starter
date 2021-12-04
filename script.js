d7b9a5a07273e428064011242ffe8ba472cfc4d2
Rick Rackley <93223405+safehaven1017@users.noreply.github.com>
2021-12-03 19:26:46 -0500

    Added Checkboxes

--- script.js
+++ script.js
@@ -4,14 +4,32 @@ const completeList = document.querySelector("#complete-items");
 
 for (let i = 0; i < todoItems.length; i++) {
     if (todoItems[i].completed)
-        addListItem(todoItems[i].title, "complete-item", completeList);
+        createCheckboxItem(todoItems[i].title, completeList);
     else
-        addListItem(todoItems[i].title, "incomplete-item", incompleteList);
+        createCheckboxItem(todoItems[i].title, incompleteList);
 }
 
-function addListItem (textContent, liClass, parentList) {
-    const newListItem = document.createElement("li");
-    newListItem.setAttribute("class", liClass);
-    newListItem.innerHTML = textContent;
-    parentList.appendChild(newListItem);
+function createCheckboxItem (str, parent) {
+    const checkboxDiv = document.createElement("div");
+    checkboxDiv.setAttribute("class", "form-check");
+    parent.appendChild(checkboxDiv);
+    const input = document.createElement("input");
+    input.setAttribute("class","form-check-input");
+    input.setAttribute("type","checkbox");
+    input.setAttribute("value","");
+    input.setAttribute("id","flexCheckDefault");
+    checkboxDiv.appendChild(input);
+    const label = document.createElement("label");
+    label.setAttribute("class","form-check-label");
+    label.setAttribute("for","flexCheckDefault");
+    label.innerHTML = str;
+    checkboxDiv.appendChild(label);
+    return label;
 }
+
+// function addTodoItem (textContent, liClass, parent) {
+//     const newListItem = document.createElement("li");
+//     newListItem.setAttribute("class", liClass);
+//     newListItem.innerHTML = textContent;
+//     parent.appendChild(newListItem);
+// }
